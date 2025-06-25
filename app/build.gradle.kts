d012c51cabdee607f1b866b55dbd481f7d6d194d
Eddy Gorbunov <edijs.gorbunovs@testdevlab.com>
2025-06-25 17:09:39 +0300

    Updated gradle libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.amazon.ivs.livetovod"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.amazon.ivs.livetovod"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
